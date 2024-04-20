41ceaf0f28a32bb43d59193bd9f0d6f30cc24c12
Victor Prouff <contact@victorprouff.fr>
2024-04-20 14:47:31 +0200

    Extract ProcessLine

--- c#/birthday-greeting/Program.cs
+++ c#/birthday-greeting/Program.cs
@@ -8,46 +8,24 @@ public static class Program
 
         try
         {
+            // Chargement fichier
             var lines = File.ReadAllLines(fileName);
 
             Console.WriteLine("Reading file...");
             var firstLine = true;
+            // Boucle sur toutes les lignes
             foreach (var line in lines)
             {
                 try
                 {
+                    // Si première ligne ne pas faire le traitement
                     if (firstLine)
                     {
                         firstLine = false;
                     }
                     else
                     {
-                        var tokens = line.Split(',');
-                        for (var token = 0; token < tokens.Length; token++)
-                            tokens[token] = tokens[token].Trim();
-
-                        if (tokens.Length == 4)
-                        {
-                            var date = tokens[2].Split('/');
-                            if (date.Length == 3)
-                            {
-                                var cal = DateTime.Now;
-
-                                if (cal.Day == int.Parse(date[0]) && cal.Month == int.Parse(date[1]))
-                                {
-                                    SendEmail(tokens[3], "Joyeux Anniversaire !",
-                                        "Bonjour " + tokens[0] + ",\nJoyeux Anniversaire !\nA bientôt,");
-                                }
-                            }
-                            else
-                            {
-                                throw new Exception("Cannot read birthdate for " + tokens[0] + " " + tokens[1]);
-                            }
-                        }
-                        else
-                        {
-                            throw new Exception("Invalid file format");
-                        }
+                        ProcessLine(line);
                     }
                 }
                 catch (Exception e)
@@ -68,6 +46,45 @@ public static class Program
         }
 
         Console.ReadLine();
+    }
+
+    private static void ProcessLine(string line)
+    {
+        // Découpe chaque ligne en éléments
+        var tokens = line.Split(',');
+
+        // Nettoie les éléments en supprimant les espaces
+        for (var token = 0; token < tokens.Length; token++)
+            tokens[token] = tokens[token].Trim();
+
+        // S'il y a 4 éléments on fait le traitement sinon on throw une exception
+        if (tokens.Length == 4)
+        {
+            // On récupère la date
+            var date = tokens[2].Split('/');
+
+            // Si la date ne contient pas 3 éléments on throw une exeption
+            if (date.Length == 3)
+            {
+                // On récupère la date du jour
+                var currentDate = DateTime.Now;
+
+                // Si la date d'anniversaire correspond à la date du jour alors on envoie un email
+                if (currentDate.Day == int.Parse(date[0]) && currentDate.Month == int.Parse(date[1]))
+                {
+                    SendEmail(tokens[3], "Joyeux Anniversaire !",
+                        "Bonjour " + tokens[0] + ",\nJoyeux Anniversaire !\nA bientôt,");
+                }
+            }
+            else
+            {
+                throw new Exception("Cannot read birthdate for " + tokens[0] + " " + tokens[1]);
+            }
+        }
+        else
+        {
+            throw new Exception("Invalid file format");
+        }
     }
 
     private static void SendEmail(string to, string title, string body)
